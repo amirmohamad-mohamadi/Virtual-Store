8698ff977bbe1a13f5b0362a343fe7980732528e
amirmohamad-mohamadi <amirmm5560@gmail.com>
2025-08-30 01:39:14 +0330

    Initial setup and root path configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Prostore",
-  description: "A modern store built with Next.js, ShadCN, and Prisma.",
-};
+// ? %s is a placeholder for dynamic page titles
+// ? It will be replaced by each page's specific title at runtime
+// ! Example: "Dashboard" becomes "Dashboard | MyApp"
+// * This helps maintain consistent branding across all pages
 
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
 export default function RootLayout({
   children,
 }: Readonly<{
