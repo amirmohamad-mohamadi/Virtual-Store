df86d0f628f8a03f7e9cb552087c2b2f415ab774
amirmohamad-mohamadi <amirmm5560@gmail.com>
2025-09-12 06:43:10 +0330

    add initial user records for development and testing

--- db/seed.ts
+++ db/seed.ts
@@ -6,9 +6,14 @@ async function main() {
 
   // SQLCode => DELETE FROM "Product";
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   // SQLCode => INSERT INTO "Product" ("name", "price", "stock", ...) VALUES  ('Phone', 499.99, 10, ...), ('Laptop', 999.99, 5, ...),...;
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully");
 }
