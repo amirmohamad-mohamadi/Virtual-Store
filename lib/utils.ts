7110e69f91bddc8ed720b6ed4d8b2212d1a2e5ed
amirmohamad-mohamadi <amirmm5560@gmail.com>
2025-09-28 05:56:10 +0330

    add formatError helper to handle Zod and Prisma errors with readable messages

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,34 @@ export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
+
+// Format Errors
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any): string {
+  // Check if the error is a Zod validation error => signUpFormSchema => lib\validator.ts
+  if (error.name === "ZodError") {
+    // Handle Zod error
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const message = error.errors[field].message;
+      return typeof message === "string" ? message : JSON.stringify(message);
+    });
+
+    return fieldErrors.join(". ");
+  }
+  // Check if the error is a Prisma unique constraint error => prisma from db
+  // uniq email in db
+  else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    // Get the field name that caused the uniqueness violation
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    // Return a user-friendly message like "Email already exists"
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    // Handle other errors
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
